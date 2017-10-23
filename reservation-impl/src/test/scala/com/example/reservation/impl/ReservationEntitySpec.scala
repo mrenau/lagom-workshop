9a63e3c842bbd081090bccf09f9763ba080eafe2
James Roper <james@jazzy.id.au>
2017-10-24 07:54:20 +1100

    Added second reservation entity spec

--- reservation-impl/src/test/scala/com/example/reservation/impl/ReservationEntitySpec.scala
+++ reservation-impl/src/test/scala/com/example/reservation/impl/ReservationEntitySpec.scala
@@ -6,6 +6,7 @@ import java.util.UUID
 
 import akka.actor.ActorSystem
 import com.example.common.Reservation
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity.InvalidCommandException
 import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
 import com.lightbend.lagom.scaladsl.testkit.PersistentEntityTestDriver
 import org.scalatest.{BeforeAndAfterAll, Inside, Matchers, WordSpec}
@@ -47,6 +48,21 @@ class ReservationEntitySpec extends WordSpec with Matchers with BeforeAndAfterAl
       outcome.state should ===(ReservationState(Seq(reservation)))
     }
 
+    "not allow adding overlapping reservations" in withDriver { driver =>
+
+      val reservation1 = Reservation(LocalDate.now, LocalDate.now.plus(2, ChronoUnit.DAYS))
+      val reservation2 = Reservation(LocalDate.now.plus(1, ChronoUnit.DAYS), LocalDate.now.plus(3, ChronoUnit.DAYS))
+
+      driver.run(AddReservation(reservation1))
+
+      val outcome = driver.run(AddReservation(reservation2))
+      outcome.events should have size 0
+      inside(outcome.replies.head) {
+        case error: InvalidCommandException =>
+          error.getMessage should ===("Listing is already booked for those dates")
+      }
+    }
+    
   }
 
 }
