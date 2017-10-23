2027bd35411e612101491477cb1ecd6609c4b1d1
James Roper <james@jazzy.id.au>
2017-10-24 06:29:58 +1100

    Added the reservation entity

--- reservation-impl/src/main/scala/com/example/reservation/impl/ReservationEntity.scala
+++ reservation-impl/src/main/scala/com/example/reservation/impl/ReservationEntity.scala
@@ -1,10 +1,13 @@
 package com.example.reservation.impl
 
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
 import java.util.UUID
 
 import com.example.common.Reservation
+import com.lightbend.lagom.scaladsl.api.transport.BadRequest
 import com.lightbend.lagom.scaladsl.persistence.PersistentEntity.ReplyType
-import com.lightbend.lagom.scaladsl.persistence.{AggregateEvent, AggregateEventTag}
+import com.lightbend.lagom.scaladsl.persistence.{AggregateEvent, AggregateEventTag, PersistentEntity}
 import com.lightbend.lagom.scaladsl.playjson.{JsonSerializer, JsonSerializerRegistry}
 import play.api.libs.json._
 
@@ -134,4 +137,76 @@ object ReservationSerializerRegistry extends JsonSerializerRegistry {
   )
 }
 
+/**
+  * Reservation persistent entity.
+  */
+class ReservationEntity extends PersistentEntity {
+
+  // The types of the command, event and state
+  override type Command = ReservationCommand[_]
+  override type Event = ReservationEvent
+  override type State = ReservationState
+
+  /**
+    * Convenient shortcut that parsies the listing UUID from the
+    * entity id.
+    */
+  private lazy val listingId = UUID.fromString(entityId)
+
+  /**
+    * When a command arrives for an entity that has no events, this state
+    * will be used.
+    */
+  override def initialState: ReservationState = ReservationState.empty
+
+  /**
+    * The behaviour for this entity.
+    */
+  override def behavior: Behavior = {
+
+    // The behaviour can be defined like a state machine. In this case,
+    // we only really have one state, so we just match on that.
+    case ReservationState(reservations) =>
+
+      // Handle the add reservation command.
+      Actions().onCommand[AddReservation, ReservationAdded] {
+        case (AddReservation(reservation), ctx, _) =>
+
+          // Validate start/end dates
+          val now = LocalDate.now()
+          if (reservation.checkout.isBefore(reservation.checkin) || reservation.checkout == reservation.checkin) {
+            ctx.commandFailed(BadRequest("Checkout date must be after checkin date"))
+            ctx.done
+          } else if (reservation.checkin.isBefore(now)) {
+            ctx.commandFailed(BadRequest("Cannot make a reservation for the past"))
+            ctx.done
+
+          // Check that it doesn't overlap with any existing reservations
+          } else if (reservations.exists(_.conflictsWith(reservation))) {
+            ctx.commandFailed(BadRequest("Listing is already booked for those dates"))
+            ctx.done
+          } else {
+
+            // If all the validation passes, persist a reservation added event
+            ctx.thenPersist(ReservationAdded(listingId, UUID.randomUUID(), reservation))(ctx.reply)
+          }
+
+      // Handle the get current reservations command
+      }.onReadOnlyCommand[GetCurrentReservations.type, Seq[Reservation]] {
+        case (GetCurrentReservations, ctx, _) =>
+          val now = LocalDate.now()
+          // Reply with all the reservations that aren't before now
+          ctx.reply(reservations.dropWhile(_.checkout.isBefore(now)))
+
+      // Handle the reservation added event
+      }.onEvent {
+        case (ReservationAdded(_, _, reservation), state) =>
+          // Create a new state with the reservation added, sorted by
+          // checkin date.
+          ReservationState((reservations :+ reservation)
+            .sortBy(_.checkin.asInstanceOf[ChronoLocalDate]))
+      }
+
+  }
+}
 
